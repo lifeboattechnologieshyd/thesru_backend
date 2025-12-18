f18c1d6981436c52c82322574716d34adbfdfb76
Karthik Narayan <contact@karthiknarayan.in>
2025-12-18 22:19:57 +0530

    Update Django settings module to production and enhance database logging

--- manage.py
+++ manage.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+
+    print("############################ DATABASES ############################")
+    print(f"PostgreSQL: {os.environ.get('POSTGRES_DB_NAME')}")
+    print("###################################################################")
+
+    # try:
+    #     ENABLE_TRACING = os.environ.get("ENABLE_TRACING")
+    #     if ENABLE_TRACING == "True":
+    #         load_traces()
+    # except:
+    #     pass
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +35,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
