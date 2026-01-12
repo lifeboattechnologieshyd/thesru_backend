d4c5fe45d1a52bfa61fcfdb78f5e22feadc34771
charanlakkam <charan@lifeboat.co.in>
2026-01-12 18:37:32 +0530

    webhook api added

--- config/settings/common.py
+++ config/settings/common.py
@@ -179,7 +179,7 @@ MEDIA_URL = f"https://{AWS_S3_BUCKET}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/"
 
 
 # CASHFREE_URL = "https://sandbox.cashfree.com/pg/orders"
-CASHFREE_API_VERSION = "2023-08-01"
+CASHFREE_API_VERSION = "2025-01-01"
 # CASHFREE_CLIENT_ID = os.getenv("CASHFREE_CLIENT_ID")
 # CASHFREE_CLIENT_SECRET = os.getenv("CASHFREE_CLIENT_SECRET")
 # CASHFREE_WEBHOOK = "https://dev-api.sru.ai/payment/paymentWebhook"
