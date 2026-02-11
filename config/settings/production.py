cc3f3ca174609fd60078805a731233c1c45ae17e
ranjith <padidalaranjith@gmail.com>
2026-02-11 23:09:56 +0530

    prod settings for cashfree changed.

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,1 +1,5 @@
 from config.settings.common import *  # noqa : F403
+
+CASHFREE_URL = "https://api.cashfree.com/pg/orders"
+CASHFREE_WEBHOOK = "https://api-ecommerce.lifeboattechnologies.com/store/paymentWebhook"
+
