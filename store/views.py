e01dcb65602dccbfa778bf29dba020d8e2342e84
ranjith <padidalaranjith@gmail.com>
2026-03-14 11:30:20 +0530

    order number issue fixed.

--- store/views.py
+++ store/views.py
@@ -939,7 +939,7 @@ class InitiateOrder(APIView):
             platform_fee = Decimal("0.00")
 
             final_amount = subtotal - coupon_discount + shipping_charge + platform_fee
-            order_number = generate_order_number(store, "SRU")
+            order_number = generate_order_number(store, store.product_code)
             order = Order.objects.create(
                 store=store,
                 user=user,
