3426e512718385e08a6d113115d478bc9591006e
ranjith <padidalaranjith@gmail.com>
2026-03-14 11:43:50 +0530

    payment issue fixed.

--- store/views.py
+++ store/views.py
@@ -998,7 +998,8 @@ class InitiateOrder(APIView):
                     order=order,
                     gateway="CASHFREE",
                     amount=final_amount,
-                    status=PaymentStatus.INITIATED
+                    status=PaymentStatus.INITIATED,
+                    remarks="Payment Initiated"
                 )
                 payment_resp = initiateOrder(
                     user=user,
@@ -1006,20 +1007,33 @@ class InitiateOrder(APIView):
                     order=order,
                     store=store
                 )
-                payment.session_id = payment_resp["payment_session_id"]
-                payment.cf_order_id = payment_resp["cf_order_id"]
-                payment.save(
-                    update_fields=["session_id", "cf_order_id"]
-                )
-                return CustomResponse.successResponse(
-                    data={
-                        "order_number": order.order_number,
-                        "payment_session_id": payment.session_id,
-                        "cf_order_id": payment.cf_order_id,
-                        "amount": str(final_amount)
-                    },
-                    description="Order initiated successfully"
-                )
+                if payment_resp["success"]:
+                    payment.session_id = payment_resp["payment_session_id"]
+                    payment.cf_order_id = payment_resp["cf_order_id"]
+                    payment.save(
+                        update_fields=["session_id", "cf_order_id"]
+                    )
+                    return CustomResponse.successResponse(
+                        data={
+                            "order_number": order.order_number,
+                            "payment_session_id": payment.session_id,
+                            "cf_order_id": payment.cf_order_id,
+                            "amount": str(final_amount)
+                        },
+                        description="Order initiated successfully"
+                    )
+                else:
+                    payment.status = PaymentStatus.FAILED
+                    payment.remarks = str(payment.remarks) + "==" + "PaymentStatus.FAILED" + ": " + payment_resp["description"]
+                    payment.save(
+                        update_fields=["status", "remarks"]
+                    )
+                    order.status = OrderStatus.FAILED
+                    order.save()
+                    return CustomResponse.errorResponse(
+                        data={},
+                        description=payment_resp["description"]
+                    )
         except Exception as e:
             return CustomResponse().errorResponse(
                 description=str(e) or "Failed to initiate order"
@@ -1073,18 +1087,26 @@ def initiateOrder(user, amount, order, store):
 
             if order_id and session_id:
                 return {
+                    "success": True,
                     "cf_order_id": order_id,
                     "payment_session_id": session_id,
                     "order_number":order.order_number
                 }
             else:
-                raise Exception("Could not found cf_order_id and payment_session_id")
+                return {
+                    "success": False,
+                    "description": "Could not found cf_order_id and payment_session_id"
+                }
         else:
-            raise Exception(
-                f"Cashfree response code {response.status_code}: {response.text}"
-            )
+            return {
+                "success": False,
+                "description": f"Cashfree response code {response.status_code}: {response.text}"
+            }
     except Exception as e:
-        raise Exception(e)
+        return {
+            "success": False,
+            "description": "Exception occurred"
+        }
 
 
 SYSTEM_UPDATED_BY = "CASHFREE_WEBHOOK"
