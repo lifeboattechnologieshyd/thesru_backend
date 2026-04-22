331d86a4bfaff548ea11112e0c0acbce22cefdf6
charanlakkam <charan@schoolfirst.ai>
2026-04-22 16:24:33 +0530

    BusinessOnboardingAPIView changes

--- utils/store.py
+++ utils/store.py
@@ -230,7 +230,7 @@ def get_phonepe_token():
 def create_phonepe_payment(obj):
     token = get_phonepe_token()
 
-    url = "https://api-preprod.phonepe.com/apis/pg-sandbox/v1/oauth/token"
+    url = "https://api-preprod.phonepe.com/apis/pg-sandbox/v1/payments"
 
     headers = {
         "Authorization": f"Bearer {token}",
